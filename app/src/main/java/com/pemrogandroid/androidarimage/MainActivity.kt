90e181923ca49e00242ab8a0ea9d3387c2933a8c
Gerard <gerardjanno@gmail.com>
2023-11-08 12:26:28 -0800

    b  54

--- app/src/main/java/com/pemrogandroid/androidarimage/MainActivity.kt
+++ app/src/main/java/com/pemrogandroid/androidarimage/MainActivity.kt
@@ -28,10 +28,9 @@ import io.github.sceneview.ar.node.AugmentedImageNode
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var sceneView:ArSceneView
-
-    lateinit var videoNode: VideoNode
-    lateinit var mediaPlayer: MediaPlayer
+    lateinit var sceneView:ArSceneView // Create a reference to the AR scene view
+    lateinit var videoNode: VideoNode // Create a reference to the VideoNode for playing video
+    lateinit var mediaPlayer: MediaPlayer // Create a reference to the MediaPlayer for handling video playback
 
     /*
         - ArModelNode is a class from the SceneView library for Android AR development.
@@ -43,42 +42,49 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)// Set the content view to the main layout
 
+        // Initialize the AR scene view and configure it
         sceneView = findViewById<ArSceneView?>(R.id.SceneView).apply {
-            this.lightEstimationMode = Config.LightEstimationMode.DISABLED
-            this.planeRenderer.isVisible = true
+            this.lightEstimationMode = Config.LightEstimationMode.DISABLED //Disable light estimation
+            this.planeRenderer.isVisible = true // Make the plane renderer visible
         }
 
+        // Initialize the MediaPlayer and load a video from resources
         mediaPlayer= MediaPlayer.create(this,R.raw.videoadv)
 
+        // Add an AugmentedImageNode to the scene view, representing the target image
         sceneView.addChild(AugmentedImageNode(
             engine = sceneView.engine,
             imageName = "video",
             bitmap = this.assets.open("models/kartuNama.png")
                 .use(BitmapFactory::decodeStream),
             onUpdate = { node, _ ->
-                if (node.isTracking) {
+                if (node.isTracking) {// If the image is being tracked
                     if (!videoNode.player.isPlaying) {
-                        sceneView.planeRenderer.isVisible = false
-
-                        videoNode.player.start()
-
-
+                        sceneView.planeRenderer.isVisible = false // Hide the plane renderer
+                        videoNode.player.start()// Start playing the video
                     }
-                } else {
+                } else {// If the image is not being tracked
                     if (videoNode.player.isPlaying) {
-                        sceneView.planeRenderer.isVisible = true
-                        videoNode.player.pause()
+                        sceneView.planeRenderer.isVisible = true// Show the plane renderer
+                        videoNode.player.pause()// Stop playing video
                     }
                 }
             }
         ).apply {
+            // Create a VideoNode for displaying the video
 
-            videoNode=VideoNode(engine=sceneView.engine,player = mediaPlayer, scaleToUnits = 0.2f, centerOrigin = Position(x = 0.0f, y = 0.0f, z = 0.0f),glbFileLocation = "models/plane.glb", onLoaded = { _, _->
-
+            videoNode=VideoNode(engine=sceneView.engine,
+                player = mediaPlayer,
+                scaleToUnits = 0.2f,
+                centerOrigin = Position(x = 0.0f, y = 0.0f, z = 0.0f),
+                glbFileLocation = "models/plane.glb",
+                onLoaded = { _, _->
+                // // Handle video node loaded event if needed
             })
-            addChild(videoNode)
+            addChild(videoNode)// Add the video node as a child of the AugmentedImageNode
+
         })
 
 
@@ -86,7 +92,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
-        mediaPlayer.release()
+        mediaPlayer.release()// Release the MediaPlayer resources when the activity is destroyed
     }
 
 }
